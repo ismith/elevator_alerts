ceec36e7540381dab9b2b7ed990b1b357456b26c
Ian Smith <ismith@metaforgotten.org>
2015-09-07 23:02:51 -0700

    For now, comment out Outages

--- worker.rb
+++ worker.rb
@@ -8,17 +8,17 @@ class BartWorker
       Models::Elevators.first_or_create(:name => elevator.name)
     end
 
-    # End outages that are over
-    outages_to_end = Models::Outage.all(:ended_at => nil,
-                                        :elevator.not => out_elevators)
-    outages_to_end.each(&:end!)
-
-    # Open outaages that need opening
-    out_elevators.select do |e|
-      Models::Outage.first(:ended_at => nil,
-                           :elevator => e).nil?
-    end.map do |e|
-      Models::Outage.create(:elevator => e)
-    end
+#    # End outages that are over
+#    outages_to_end = Models::Outage.all(:ended_at => nil,
+#                                        :elevator.not => out_elevators)
+#    outages_to_end.each(&:end!)
+#
+#    # Open outaages that need opening
+#    out_elevators.select do |e|
+#      Models::Outage.first(:ended_at => nil,
+#                           :elevator => e).nil?
+#    end.map do |e|
+#      Models::Outage.create(:elevator => e)
+#    end
   end
 end
