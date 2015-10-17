324a09ea19d8ed0eefb602bc3f4c2005458c4810
Ian Smith <ismith@metaforgotten.org>
2015-10-17 13:59:03 -0700

    Exclude SignalException errors from rollbar (since heroku sigterms on deploy)
    Also,disable rollbar in testing

--- initializer.rb
+++ initializer.rb
@@ -1,6 +1,9 @@
 require 'rollbar'
 
-Rollbar.configure do |c|
-  c.access_token = ENV["ROLLBAR_ACCESS_TOKEN"]
-  c.endpoint = ENV["ROLLBAR_ENDPOINT"]
+unless ENV['RACK_ENV']=='testing'
+  Rollbar.configure do |c|
+    c.access_token = ENV["ROLLBAR_ACCESS_TOKEN"]
+    c.endpoint = ENV["ROLLBAR_ENDPOINT"]
+    c.exception_level_filters.merge!('SignalException' => 'ignore')
+  end
 end
